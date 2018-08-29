c153410ca4c3637322b9e8d391d70d00942b82fc
arthur silva <arxdsilva@gmail.com>
2018-08-29 20:06:56 -0300

    postgre: add simple db that implements storage

---
+++ storage/postgre/postgre.go
@@ -0,0 +1,33 @@
+package postgre
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+type Postgre struct {
+	db *sql.DB
+}
+
+func New() (postg *Postgre, err error) {
+	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbconfig := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", user, dbname, password, host)
+	db, err := sql.Open("postgres", dbconfig)
+	if err != nil {
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		return
+	}
+	postg.db = db
+	return
+}
+
+func SaveCall() error {
+	return nil
+}
