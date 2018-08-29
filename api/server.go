1a36add09abc1d60fd539faf18199d888b447c76
arthur silva <arxdsilva@gmail.com>
2018-08-29 20:04:54 -0300

    api: add basic server

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/arxdsilva/olist/storage"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type Server struct {
+	Storage storage.Storage
+	Port    string
+}
+
+func New() *Server {
+	return &Server{Port: port()}
+}
+
+func (s *Server) Listen() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.GET("/call", call)
+	e.POST("/call", call)
+	e.Logger.Fatal(e.Start(s.Port))
+}
+
+func port() (p string) {
+	if p = os.Getenv("ADDRS_PORT"); p != "" {
+		return fmt.Sprintf(":%s", p)
+	}
+	return ":8080"
+}
