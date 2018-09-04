537b4e49d1c2688b4129e5a0bfbb3de12ef97f56
arthur silva <arxdsilva@gmail.com>
2018-09-04 10:40:38 -0300

    bill: add error on Calculate

--- bill/bill.go
+++ bill/bill.go
@@ -8,9 +8,9 @@ import (
 // of the time to be charged by the telephone records
 // 600 > 2200: 0.36 + 0.09/min
 // 2200 > 600: 0.36
-func Calculate(start, end time.Time) (r float64) {
+func Calculate(start, end time.Time) (r float64, err error) {
 	timeUsed := end.Sub(start)
 	minutesUsed := timeUsed.Minutes()
 	r = 0.36 + (0.09 * minutesUsed)
-	return r
+	return r, err
 }
