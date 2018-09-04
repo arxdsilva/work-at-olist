f510f0ff6708c84ba59a17b2e1bb75203ab7c9e1
arthur silva <arxdsilva@gmail.com>
2018-09-04 10:24:06 -0300

    add suite test

---
+++ bill/suite_test.go
@@ -0,0 +1,16 @@
+package bill
+
+import (
+	"testing"
+
+	"github.com/arxdsilva/olist/storage"
+	"gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+type S struct {
+	db *storage.Storage
+}
+
+var _ = check.Suite(&S{})
