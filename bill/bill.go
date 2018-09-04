37ae5a3e334b779d2a10e51a2674e3e93b919d5d
arthur silva <arxdsilva@gmail.com>
2018-09-04 10:33:05 -0300

    bill: add docs to Calculate

--- bill/bill.go
+++ bill/bill.go
@@ -4,6 +4,10 @@ import (
 	"time"
 )
 
+// Calculate makes the math
+// of the time to be charged by the telephone records
+// 600 > 2200: 0.36 + 0.09/min
+// 2200 > 600: 0.36
 func Calculate(start, end time.Time) (r float32) {
 
 	return r
