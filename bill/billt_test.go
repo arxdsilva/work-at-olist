633314795e49f39ffdec39cb29ad2f050165886d
arthur silva <arxdsilva@gmail.com>
2018-09-04 10:41:00 -0300

    bill: fix TestCalculateBillOnNormalHours to expect error
    from Calculate

--- bill/billt_test.go
+++ bill/billt_test.go
@@ -9,7 +9,8 @@ import (
 func (s *S) TestCalculateBillOnNormalHours(c *check.C) {
 	start := time.Date(2018, 9, 4, 14, 0, 0, 651387237, time.UTC)
 	end := time.Date(2018, 9, 4, 20, 0, 0, 651387237, time.UTC)
-	toPay := Calculate(start, end)
+	toPay, err := Calculate(start, end)
+	c.Assert(err, check.Equals, nil)
 	c.Assert(toPay, check.Equals, 32.76)
 }
 
