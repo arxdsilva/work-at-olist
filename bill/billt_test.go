4acb63213f4636c16756c51c81ecd7d222cca103
arthur silva <arxdsilva@gmail.com>
2018-09-04 16:13:57 -0300

    bill: fix ns in tests

--- bill/billt_test.go
+++ bill/billt_test.go
@@ -7,23 +7,23 @@ import (
 )
 
 func (s *S) TestCalculateBillOnNormalHours(c *check.C) {
-	start := time.Date(2018, 9, 4, 14, 0, 0, 651387237, time.UTC)
-	end := time.Date(2018, 9, 4, 20, 0, 0, 651387237, time.UTC)
+	start := time.Date(2018, 9, 4, 14, 0, 0, 0, time.UTC)
+	end := time.Date(2018, 9, 4, 20, 0, 0, 0, time.UTC)
 	toPay, err := Calculate(start, end)
 	c.Assert(err, check.Equals, nil)
 	c.Assert(toPay, check.Equals, 32.76)
 }
 
 func (s *S) TestCalculateBillOnInvalidEndTime(c *check.C) {
-	start := time.Date(2018, 9, 4, 20, 0, 0, 651387237, time.UTC)
-	end := time.Date(2018, 9, 4, 14, 0, 0, 651387237, time.UTC)
+	start := time.Date(2018, 9, 4, 20, 0, 0, 0, time.UTC)
+	end := time.Date(2018, 9, 4, 14, 0, 0, 0, time.UTC)
 	_, err := Calculate(start, end)
 	c.Assert(err, check.NotNil)
 }
 
 func (s *S) TestCalculateBillOnLowerRateHours(c *check.C) {
-	start := time.Date(2018, 9, 4, 22, 1, 0, 651387237, time.UTC)
-	end := time.Date(2018, 9, 5, 5, 1, 0, 651387237, time.UTC)
+	start := time.Date(2018, 9, 4, 22, 1, 0, 0, time.UTC)
+	end := time.Date(2018, 9, 5, 5, 1, 0, 0, time.UTC)
 	toPay, err := Calculate(start, end)
 	c.Assert(err, check.IsNil)
 	c.Assert(toPay, check.Equals, 0.36)
