81afc6a7a5bd1d8e512ec153c6933f13b84e26ce
arthur silva <arxdsilva@gmail.com>
2018-09-04 10:44:44 -0300

    bill: fix Calculate to recognize invalid call times

--- bill/bill.go
+++ bill/bill.go
@@ -1,6 +1,7 @@
 package bill
 
 import (
+	"errors"
 	"time"
 )
 
@@ -9,6 +10,9 @@ import (
 // 600 > 2200: 0.36 + 0.09/min
 // 2200 > 600: 0.36
 func Calculate(start, end time.Time) (r float64, err error) {
+	if inv := start.Sub(end); inv.Minutes() > 0 {
+		return r, errors.New("Start time cannot be after the call has ended")
+	}
 	timeUsed := end.Sub(start)
 	minutesUsed := timeUsed.Minutes()
 	r = 0.36 + (0.09 * minutesUsed)
