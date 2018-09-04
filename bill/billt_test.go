c51536b691e24a099203987c63a57b39d60622e0
arthur silva <arxdsilva@gmail.com>
2018-09-04 10:39:52 -0300

    bill: add TestCalculateBillOnInvalidEndTime

--- bill/billt_test.go
+++ bill/billt_test.go
@@ -12,3 +12,10 @@ func (s *S) TestCalculateBillOnNormalHours(c *check.C) {
 	toPay := Calculate(start, end)
 	c.Assert(toPay, check.Equals, 32.76)
 }
+
+func (s *S) TestCalculateBillOnInvalidEndTime(c *check.C) {
+	start := time.Date(2018, 9, 4, 20, 0, 0, 651387237, time.UTC)
+	end := time.Date(2018, 9, 4, 14, 0, 0, 651387237, time.UTC)
+	_, err := Calculate(start, end)
+	c.Assert(err, check.NotNil)
+}
