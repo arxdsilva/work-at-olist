5008ffe327d67b7a8a9427f1a9e1f1dfc7e49c90
arthur silva <arxdsilva@gmail.com>
2018-09-05 18:06:33 -0300

    bill: add TestStartsAfterUnbillableHoursNextDay

--- bill/billt_test.go
+++ bill/billt_test.go
@@ -41,3 +41,8 @@ func (s *S) TestStartsAfterUnbillableHours(c *check.C) {
 	start := time.Date(2018, time.September, 4, 23, 0, 0, 0, time.UTC)
 	c.Assert(startsAfterUnbillableHours(start), check.Equals, true)
 }
+
+func (s *S) TestStartsAfterUnbillableHoursNextDay(c *check.C) {
+	start := time.Date(2018, time.September, 4, 2, 0, 0, 0, time.UTC)
+	c.Assert(startsAfterUnbillableHours(start), check.Equals, true)
+}
