073a33f59a3219b08e20337c112e957fccb8d381
arthur silva <arxdsilva@gmail.com>
2018-09-06 18:51:17 -0300

    api: create saveRecord handler

--- api/app.go
+++ api/app.go
@@ -9,3 +9,7 @@ import (
 func call(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
+
+func saveRecord(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
--- api/server.go
+++ api/server.go
@@ -22,7 +22,7 @@ func (s *Server) Listen() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.GET("/call", call)
-	e.POST("/call", call)
+	e.POST("/records", saveRecord)
 	e.Logger.Fatal(e.Start(s.Port))
 }
 
