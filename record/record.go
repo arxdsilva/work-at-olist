14afd975431f7e8c87ce246c3ffa873b950048e9
arthur silva <arxdsilva@gmail.com>
2018-09-06 20:27:34 -0300

    record: add DataChecks method

--- record/record.go
+++ record/record.go
@@ -1,5 +1,7 @@
 package record
 
+import "errors"
+
 type Record struct {
 	ID          string `json:"id"`
 	Type        string `json:"type"`
@@ -7,4 +9,20 @@ type Record struct {
 	CallID      string `json:"call_id"`
 	Source      int    `json:"source"`
 	Sestination int    `json:"destination"`
+}
+
+func (r *Record) DataChecks() (err error) {
+	if r.ID == "" {
+		return errors.New("Record ID cannot be nil")
+	}
+	if (r.Type == "") || ((r.Type != "start") && (r.Type != "end")) {
+		return errors.New("Invalid record type")
+	}
+	if r.TimeStamp == "" {
+		return errors.New("Record time stamp cannot be nil")
+	}
+	if r.CallID == "" {
+		return errors.New("Record call_id cannot be nil")
+	}
+	return
 }
