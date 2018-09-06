8256bb40207348cdcb68467bcc3ecf0cb4e22642
arthur silva <arxdsilva@gmail.com>
2018-09-06 20:47:33 -0300

    api: implement DataChecks method on saveRecord handler

--- api/app.go
+++ api/app.go
@@ -3,6 +3,7 @@ package api
 import (
 	"net/http"
 
+	"github.com/arxdsilva/olist/record"
 	"github.com/labstack/echo"
 )
 
@@ -10,6 +11,15 @@ func call(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
-func saveRecord(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func saveRecord(c echo.Context) (err error) {
+	r := new(record.Record)
+	err = c.Bind(r)
+	if err != nil {
+		return
+	}
+	err = r.DataChecks()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, r)
 }
