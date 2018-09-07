67837cdd9618da966263131769b0f52415d5dceb
arthur silva <arxdsilva@gmail.com>
2018-09-06 22:59:00 -0300

    main: fix typo on package import

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"github.com/arxdsilva/olist/api"
-	"github.com/arxdsilva/olist/storage/postgre"
 	"log"
+
+	"github.com/arxdsilva/olist/api"
+	"github.com/arxdsilva/olist/storage/postgres"
 )
 
 func main() {
 	s := api.New()
-	s.Storage, err := postgre.New()
+	storage, err := postgres.New()
 	if err != nil {
 		log.Fatal(err)
 	}
+	s.Storage = storage
 	s.Listen()
 }
