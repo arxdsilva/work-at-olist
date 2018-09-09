e393d117621e185b3533ea5d98a0bac76aa6d5f7
arthur silva <arxdsilva@gmail.com>
2018-09-09 15:26:11 -0300

    storage: implement fake SaveCalls and SaveBill

--- storage/fakestorage.go
+++ storage/fakestorage.go
@@ -1,12 +1,16 @@
 package storage
 
 import (
+	"errors"
+
 	"github.com/arxdsilva/olist/bill"
 	"github.com/arxdsilva/olist/record"
 )
 
 type FakeStorage struct {
 	records []record.Record
+	bills   []bill.Bill
+	calls   []bill.Call
 }
 
 func (f FakeStorage) SaveRecord(r record.Record) (err error) {
@@ -19,7 +23,7 @@ func (f FakeStorage) UUIDFromStart(r record.Record) (uuid string, err error) {
 }
 
 func (f FakeStorage) BillFromID(id string) (b bill.Bill, err error) {
-	return
+	return b, errors.New("Not found")
 }
 
 func (f FakeStorage) CallsFromBillID(id string) (cs []bill.Call, err error) {
@@ -31,9 +35,13 @@ func (f FakeStorage) RecordsFromBill(b bill.Bill) (rs []record.Record, err error
 }
 
 func (f FakeStorage) SaveBill(b bill.Bill) (err error) {
+	f.bills = append(f.bills, b)
 	return
 }
 
 func (f FakeStorage) SaveCalls(c []bill.Call) (err error) {
+	for _, call := range c {
+		f.calls = append(f.calls, call)
+	}
 	return
 }
