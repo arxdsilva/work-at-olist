08f5eb44d58eca0edf2049b401ba4e3035186689
arthur silva <arxdsilva@gmail.com>
2018-09-09 13:23:40 -0300

    api: implement bill handler

--- api/bill.go
+++ api/bill.go
@@ -1,8 +1,14 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/arxdsilva/olist/record"
+
+	"github.com/arxdsilva/olist/bill"
 	"github.com/labstack/echo"
 )
 
@@ -14,5 +20,91 @@ func (s *Server) Bill(c echo.Context) (err error) {
 	sub := c.Param("subscriber")
 	month := c.QueryParam("month")
 	year := c.QueryParam("year")
-	return c.JSON(http.StatusOK, sub+month+year)
+	if (month == "") || (year == "") {
+		month = string(time.Now().Month() - 1)
+		year = string(time.Now().Year())
+	}
+	bill := bill.New(month, year, sub)
+	storedBill, err := s.Storage.BillFromID(bill.ID)
+	// bill already exists
+	if err == nil {
+		calls, err := s.Storage.CallsFromBillID(storedBill.ID)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+		storedBill.Calls = calls
+		return c.JSON(http.StatusOK, storedBill)
+	}
+	// create new bill
+	records, err := s.Storage.RecordsFromBill(bill)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	m, err := strconv.Atoi(month)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	calls, err := calculateCallsPriceFromRecords(records, m)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	bill.Calls = calls
+	return c.JSON(http.StatusOK, bill)
+}
+
+func calculateCallsPriceFromRecords(rs []record.Record, monthOfReference int) (cs []bill.Call, err error) {
+	f := filterRecordsPeriod(rs, monthOfReference)
+	for _, v := range f {
+		c, errC := callFromRecords(v)
+		if errC != nil {
+			return nil, errC
+		}
+		cs = append(cs, c)
+	}
+	return
+}
+
+func callFromRecords(rs []record.Record) (c bill.Call, err error) {
+	var buff map[string]record.Record
+	for _, v := range rs {
+		buff[v.Type] = v
+	}
+	startTime, err := time.Parse(time.RFC3339, buff["start"].TimeStamp)
+	if err != nil {
+		return
+	}
+	endTime, err := time.Parse(time.RFC3339, buff["end"].TimeStamp)
+	if err != nil {
+		return
+	}
+	price, err := bill.Calculate(startTime, endTime)
+	if err != nil {
+		return
+	}
+	duration := endTime.Sub(startTime)
+	startTimeFormated := fmt.Sprintf("%v:%v:%v", startTime.Hour(), startTime.Minute(), startTime.Second())
+	c.CallPrice = price
+	c.CallStartDate = startTime.Day()
+	c.Destination = buff["start"].Destination
+	c.CallStartTime = startTimeFormated
+	c.CallDuration = fmt.Sprintf("%vh%vm%vs", duration.Hours(), duration.Minutes(), duration.Seconds())
+	return
+}
+
+func filterRecordsPeriod(rs []record.Record, monthOfReference int) (rFiltered map[string][]record.Record) {
+	rFiltered = make(map[string][]record.Record)
+	recordsMap := make(map[string][]record.Record)
+	for _, r := range rs {
+		recordsMap[r.CallID] = append(recordsMap[r.CallID], r)
+	}
+	for k, v := range recordsMap {
+		if (v[0].Type == "end") && (v[0].Month == monthOfReference) {
+			rFiltered[k] = append(rFiltered[k], v[0])
+			rFiltered[k] = append(rFiltered[k], v[1])
+		} else if (v[1].Type == "end") && (v[1].Month == monthOfReference) {
+			rFiltered[k] = append(rFiltered[k], v[0])
+			rFiltered[k] = append(rFiltered[k], v[1])
+		}
+	}
+	return
 }
