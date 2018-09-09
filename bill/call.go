dd724195836e81e713ba43ca2c0e24c679263184
arthur silva <arxdsilva@gmail.com>
2018-09-09 13:23:51 -0300

    bill: use destination as string

--- bill/call.go
+++ bill/call.go
@@ -2,7 +2,7 @@ package bill
 
 type Call struct {
 	BillID        string
-	Destination   int     `json:"destination"`
+	Destination   string  `json:"destination"`
 	CallStartDate int     `json:"start_date"`
 	CallStartTime string  `json:"start_time"`
 	CallDuration  string  `json:"duration"` // 0h22m12s
