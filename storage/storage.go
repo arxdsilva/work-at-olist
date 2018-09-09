a80a7880516bf863d91a76370ed9f33010511028
arthur silva <arxdsilva@gmail.com>
2018-09-09 11:19:44 -0300

    storage: add BillFromID to interface

--- storage/storage.go
+++ storage/storage.go
@@ -1,10 +1,12 @@
 package storage
 
 import (
+	"github.com/arxdsilva/olist/bill"
 	"github.com/arxdsilva/olist/record"
 )
 
 type Storage interface {
 	SaveRecord(record.Record) error
 	UUIDFromStart(record.Record) (string, error)
+	BillFromID(string) (bill.Bill, error)
 }
